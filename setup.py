63f78c12f5e23a6e7d9b9c109e78c8df3e38888c
Daniel Pope <dan@mauveinternet.co.uk>
2012-05-13 01:12:13 +0100

    New: packaged for PyPI

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ setup(
     name='korovic',
     version='1.0',
     packages=find_packages(),
+    description="Dr. Korovic's Flying Atomic Squid",
+    long_description=open('README').read(),
+    author='Daniel Pope',
+    author_email='lord.mauve@gmail.com',
+    url='http://www.pyweek.org/e/wasabi/',
     install_requires=[
         'pyglet>=1.1.4',
         'pymunk==2.1.0',
