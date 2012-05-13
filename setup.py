107d1a024b25db88bb6ea6bbbb5e65d24d865367
Daniel Pope <dan@mauveinternet.co.uk>
2012-05-13 16:28:23 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='korovic',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(),
     description="Dr. Korovic's Flying Atomic Squid",
     long_description=open('README').read(),
